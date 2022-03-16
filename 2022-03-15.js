e8c41ae4aa00c40be624d8c52bd5462132e93189
jojdyx <jojdyx@gmail.com>
2022-03-15 20:01:30 -0400

    Coding challenges and solutions for 2022-03-15

---
+++ 2022-03-15.js
@@ -0,0 +1,17 @@
+// 8 kyu
+// Multiply
+// This code does not execute properly. Try to figure out why.
+function multiply(a, b){
+    return a * b
+}
+
+// 8 kyu
+// Even or Odd
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function even_or_odd(number) {
+    if (number % 2 == 0){
+      return 'Even'
+    } else {
+      return 'Odd'
+    }
+}
