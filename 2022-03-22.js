a3d5a9ba01813523c547ad94f6f6378d6b2ab64f
jojdyx <jojdyx@gmail.com>
2022-03-22 19:35:48 -0400

    Coding challenges and solutions for 2022-03-22

---
+++ 2022-03-22.js
@@ -0,0 +1,19 @@
+// 8 kyu
+// Convert a Boolean to a String
+// Implement a function which convert the given boolean value into its string representation.
+// Note: Only valid inputs will be given.
+function booleanToString(b){
+    if( b === true ){
+      return 'true';
+    }else{
+      return 'false';
+    }
+}
+
+// 8 kyu
+// Function 1 - hello world
+// Description:
+// Make a simple function called greet that returns the most-famous "hello world!".
+function greet(){
+    return "hello world!"
+}
