a77a0b336db6985511f50f0fcf3cae012bfbb01c
jojdyx <jojdyx@gmail.com>
2022-03-26 19:16:12 -0400

    Coding challenges and solutions for 2022-03-26

---
+++ 2022-03-26.js
@@ -0,0 +1,7 @@
+// 8 kyu
+// Function 3 - multiplying two numbers
+// Implement a function which multiplies two numbers.
+// Write here your multiply-function
+function multiply(x,y){
+    return x*y;
+}
