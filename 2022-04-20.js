4878ad6e06c99c6bee086a25c128abe0002b08b0
jojdyx <jojdyx@gmail.com>
2022-04-20 23:34:12 -0400

    Coding challenge and solution for 2022-04-20

---
+++ 2022-04-20.js
@@ -0,0 +1,11 @@
+// 8 kyu
+// Super Duper Easy
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+function problem(x){
+    //your code here
+    if(typeof x === 'number'){
+        return x*50+6
+    }else{
+        return 'Error'
+    }
+  }
