76622a4353f62dd06e35f53aac3f5f0974d9be46
jojdyx <jojdyx@gmail.com>
2022-04-22 23:53:00 -0400

    Coding challenge and solution for 2022-04-22

---
+++ 2022-04-22.js
@@ -0,0 +1,12 @@
+// 8 kyu
+// Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+// Description:
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+// Examples
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
+function replace(s){
+    return s.replace(/[aeiouAEIOU]/g, '!')
+  }
