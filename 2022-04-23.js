2434c6937035cb1ab3b238da52095b915ffed7a3
jojdyx <jojdyx@gmail.com>
2022-04-23 22:54:10 -0400

    More coding challenges and solutions for 2022-04-23

--- 2022-04-23.js
+++ 2022-04-23.js
@@ -17,3 +17,59 @@ function pipeFix(numbers){
     }
     return arr
   }
+
+// 8 kyu
+// Vowel remover
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+// don't worry about uppercase vowels
+// y is not considered a vowel for this kata
+function shortcut (string) {
+  return string.replace(/[aeiou]/g, '')
+}
+
+// 8 kyu
+// Enumerable Magic #25 - Take the First N Elements
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+function take(arr, n) {
+  return arr.slice(0,n)
+}
+
+// 8 kyu
+// A wolf in sheep's clothing
+// Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+// Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+// [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+//    7      6      5      4      3            2      1
+// If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+// Note: there will always be exactly one wolf in the array.
+// Examples
+// Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+// Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+// Input: ["sheep", "sheep", "wolf"]
+// Output: "Pls go away and stop eating my sheep"
+function warnTheSheep(queue) {
+  let wolfIndex = queue.indexOf("wolf")
+  let sheepPosition = queue.length-1-wolfIndex
+  if (wolfIndex == queue.length-1){
+    return "Pls go away and stop eating my sheep"
+  }else{
+    return `Oi! Sheep number ${sheepPosition}! You are about to be eaten by a wolf!`
+  }
+}
+
+// 8 kyu
+// Reversing Words in a String
+// You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+// As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+// Example (Input --> Output)
+// "Hello World" --> "World Hello"
+// "Hi There." --> "There. Hi"
+// Happy coding!
+function reverse(string){
+  return string.split(' ').reverse().join(' ')
+}
