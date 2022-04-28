7374ab2abc5ac642b373b84fa96efc4cf0addc1f
jojdyx <jojdyx@gmail.com>
2022-04-27 23:52:10 -0400

    More coding challenges and solutions for 2022-04-27

--- 2022-04-27.js
+++ 2022-04-27.js
@@ -7,3 +7,16 @@
 function removeEveryOther(arr){
     return arr.filter((e,i)=> i%2==0)
 }
+
+// 8 kyu
+// Get Nth Even Number
+// Return the Nth Even Number
+// Example(Input --> Output)
+// 1 --> 0 (the first even number is 0)
+// 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+// 100 --> 198
+// 1298734 --> 2597466
+// The input will not be 0.
+function nthEven(n){
+    return (n-1)*2
+}
