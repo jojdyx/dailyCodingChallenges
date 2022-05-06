60a49ea2cd9b83fcb7d9aae6adc0763c1c5752c3
jojdyx <jojdyx@gmail.com>
2022-05-05 21:30:24 -0400

    One more coding challenge and solution for 2022-05-05

--- 2022-05-05.js
+++ 2022-05-05.js
@@ -38,3 +38,17 @@ function answer3(){
 function buildString(...template){
     return `I like ${template.join(', ')}!`;
 }
+
+// 8 kyu
+// Template Strings
+// Template Strings
+// Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+// Task
+// Your task is to return the correct string using the Template String Feature.
+// Input
+// Two Strings, no validation is needed.
+// Output
+// You must output a string containing the two strings with the word ```' are '```
+var TempleStrings = function(obj, feature) {
+    return `${obj} are ${feature}`
+}
