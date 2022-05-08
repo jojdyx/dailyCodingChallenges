900f27de16298822042290250b3d3014b425af37
jojdyx <jojdyx@gmail.com>
2022-05-07 23:16:14 -0400

    More coding challenges and solutions for 2022-05-07

--- 2022-05-07.js
+++ 2022-05-07.js
@@ -13,3 +13,16 @@ function cookie(x){
       return 'Who ate the last cookie? It was the dog!'
     }
 }
+
+// 8 kyu
+// Multiply the number
+// Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+// multiply(3)==15 // 3 * 5¹
+// multiply(10)==250 // 10 * 5²
+// multiply(200)==25000 // 200 * 5³
+// multiply(0)==0 // 0 * 5¹
+// multiply(-3)==-15 // -3 * 5¹
+function multiply(number){
+    let digit = Math.abs(number).toString().length
+    return number * Math.pow(5, digit)
+}
