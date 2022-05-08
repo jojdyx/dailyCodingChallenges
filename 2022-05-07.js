1a810fcc5aded78f80a18cca6d5ee42545f38cc0
jojdyx <jojdyx@gmail.com>
2022-05-07 23:29:58 -0400

    One more coding challenges and solutions for 2022-05-07

--- 2022-05-07.js
+++ 2022-05-07.js
@@ -26,3 +26,21 @@ function multiply(number){
     let digit = Math.abs(number).toString().length
     return number * Math.pow(5, digit)
 }
+
+// 8 kyu
+// Fix the Bugs (Syntax) - My First Kata
+// Fix the Bugs (Syntax) - My First Kata
+// Overview
+// Hello, this is my first Kata so forgive me if it is of poor quality.
+// In this Kata you should fix/create a program that returns the following values:
+// false/False if either a or b (or both) are not numbers
+// a % b plus b % a if both arguments are numbers
+// You may assume the following:
+// If a and b are both numbers, neither of a or b will be 0.
+function myFirstKata(a, b) {
+    if (typeof(a) == "number" && typeof(b) == "number") {
+      return a%b + b%a;
+    } else {
+      return false;
+    }
+}
