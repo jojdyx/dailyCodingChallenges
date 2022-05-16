b461321e3467cae2137404a17993f80b0c078d81
jojdyx <jojdyx@gmail.com>
2022-05-15 22:35:00 -0400

    More coding challenges and solutions for 2022-05-15

--- 2022-05-15.js
+++ 2022-05-15.js
@@ -44,3 +44,20 @@ function equal5(){
 function animal(obj){
     return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
 }
+
+// 8 kyu
+// Training JS #10: loop statement --for
+// Task
+// Coding in function pickIt, function accept 1 parameter:arr, it's a number array, we need traverse arr by using for loop, if element is odd number, push it to array odd, if it's a even number, push it to array even.
+// I've defined two array odd and even in the function, and also wrote the return statement. your work is write a for loop.
+function pickIt(arr){
+    var odd=[],even=[];
+    for (let i = 0; i<arr.length; i++){
+      if (arr[i]%2==0 ){
+        even.push(arr[i])
+      }else{
+        odd.push(arr[i])
+      }
+    }
+    return [odd,even];
+}
