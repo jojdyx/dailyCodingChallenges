1c1a11ca8c4ea3d83698e9f1b9edda1329b40147
jojdyx <jojdyx@gmail.com>
2022-05-16 23:44:31 -0400

    More coding challenges and solutions for 2022-05-16

--- 2022-05-16.js
+++ 2022-05-16.js
@@ -9,3 +9,25 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
       return false
     }
 };
+
+// 8 kyu
+// Counting sheep...
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+// For example,
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+// Hint: Don't forget to check for bad values like null/undefined
+function countSheeps(arrayOfSheep) {
+  let arr =[]
+  for(let i = 0; i<arrayOfSheep.length; i++){
+    if(arrayOfSheep[i] == true){
+      arr.push(arrayOfSheep[i])
+    }
+  }
+  return arr.length
+}
