981cd7232abf904a77e6745727a856a08af146da
jojdyx <jojdyx@gmail.com>
2022-05-22 18:45:52 -0400

    3 8kyu kata for 2022-05-22

--- 2022-05-22.js
+++ 2022-05-22.js
@@ -21,3 +21,18 @@ function fakeBin(x){
 function strCount(str, letter){  
     return str.split('').filter(x => x==letter).length
 }
+
+// 8 kyu
+// Squash the bugs
+// Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+// There will only be one 'longest' word.
+function findLongest(str) {
+    var spl = str.split(" ");
+    var longest = 0  
+    for (var i = 0; i < spl.length; i++) {
+      if (spl[i].length > longest) {
+        longest = spl[i].length
+      }
+    }
+      return longest 
+}
