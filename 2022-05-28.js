d34afa8ed219f334b572b2b040ab0386511af8df
jojdyx <jojdyx@gmail.com>
2022-05-28 22:10:52 -0400

    3 8kyu kata for 2022-05-28

--- 2022-05-28.js
+++ 2022-05-28.js
@@ -19,3 +19,27 @@ function sumOfDifferences(arr) {
 function toFreud(string) {
     return string.length >=1 ? string.split(' ').map(x => x = 'sex').join(' ') : ''
 }
+
+// 8 kyu
+// Sum of Multiples
+// Your Job
+// Find the sum of all multiples of n below m
+// Keep in Mind
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+// Examples
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+function sumMul(n,m){
+    let result = 0;
+    if(n>=m){
+      return 'INVALID'
+    }else{
+      for(let i = n; i <= m; i+=n){
+        result += i
+      }
+      return result
+    }
+}
