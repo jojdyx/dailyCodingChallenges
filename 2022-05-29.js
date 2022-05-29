574a3a9b25cff58e975f2a0d4b5dbe4125cf85bd
jojdyx <jojdyx@gmail.com>
2022-05-29 13:48:00 -0400

    2 8kyu kata for 2022-05-29

--- 2022-05-29.js
+++ 2022-05-29.js
@@ -10,3 +10,17 @@
 function squareOrSquareRoot(array) {
     return array.map(x=> Math.sqrt(x) % 1 ? x*x : Math.sqrt(x));  
 }
+
+// 8 kyu
+// No zeros for heros
+// Numbers ending with zeros are boring.
+// They might be fun in your world, but not here.
+// Get rid of them. Only the ending ones.
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+// Zero alone is fine, don't worry about it. Poor guy anyway
+function noBoringZeros(n) {
+    return n.toString().endsWith(0) ? Number(n.toString().replace(/0+$/, '')) : n
+}
