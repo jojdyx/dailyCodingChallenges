09ef1e45acb0a80319b2ad2695737d6011c93d94
jojdyx <jojdyx@gmail.com>
2022-06-02 20:01:58 -0800

    2 8kyu kata for 2022-06-02

--- 2022-06-02.js
+++ 2022-06-02.js
@@ -13,3 +13,12 @@
 function isDigit(s) {
     return s == parseFloat(s) ? true : false
 }
+
+// 8 kyu
+// No Loops 2 - You only need one
+// *** No Loops Allowed ***
+// You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+// Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+function check(a,x){
+    return a.includes(x) ? true : false
+}
