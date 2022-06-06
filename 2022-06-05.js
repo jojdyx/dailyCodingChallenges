4670297177499c323fef5ad841991ce557a6c9ba
jojdyx <jojdyx@gmail.com>
2022-06-05 21:37:50 -0400

    2 8kyu kata for 2022-06-05

--- 2022-06-05.js
+++ 2022-06-05.js
@@ -10,3 +10,22 @@
 function hello(name) {
     return `Hello, ${name ? name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() : 'World'}!`
 }
+
+// 8 kyu
+// Generate range of integers
+// Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+// Task
+// Implement a function named
+// generateRange(2, 10, 2) // should return array of [2,4,6,8,10]
+// generateRange(1, 10, 3) // should return array of [1,4,7,10]
+// Note
+// min < max
+// step > 0
+// the range does not HAVE to include max (depending on the step)
+function generateRange(min, max, step){
+    let arr=[]
+    for(let i = min; i<=max; i+=step){
+      arr.push(i)
+    }
+      return arr
+}
