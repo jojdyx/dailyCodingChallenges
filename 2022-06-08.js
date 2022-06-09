9066e456f2a55d4fd0a7e219970fac58b04f57e9
jojdyx <jojdyx@gmail.com>
2022-06-08 23:43:40 -0400

    1 8kyu kata for 2022-06-08

---
+++ 2022-06-08.js
@@ -0,0 +1,7 @@
+// 8 kyu
+// Remove duplicates from list
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+// The order of the sequence has to stay the same.
+function distinct(a) {
+    return a.filter((e,i,a)=> a.indexOf(e)==i);
+}
