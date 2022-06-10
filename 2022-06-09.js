debea5ef046466ea6098cf66bffb3d79b87e5bc6
jojdyx <jojdyx@gmail.com>
2022-06-09 23:42:02 -0400

    1 8kyu & 1 7kyu kata for 2022-06-09

--- 2022-06-09.js
+++ 2022-06-09.js
@@ -12,3 +12,12 @@
 function reverseLetter(str) {
     return str.match(/[a-z]/g, '').reverse().join('')
 }
+
+// 8 kyu
+// Find Nearest square number
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+function nearestSq(n){
+    return Math.pow(Math.round(Math.sqrt(n)),2)
+}
+
+
