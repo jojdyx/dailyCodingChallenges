93f04d97577e231a5a639eb36883d89a3660456e
jojdyx <jojdyx@gmail.com>
2022-06-13 23:24:04 -0400

    1 7kyu & 1 6kyu kata for 2022-06-13

--- 2022-06-13.js
+++ 2022-06-13.js
@@ -8,3 +8,11 @@
 function descendingOrder(n){
     return Number(n.toString().split('').sort((a,b)=> b-a).join(''))
 }
+
+// 6 kyu
+// Bit Counting
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+var countBits = function(n) {
+    return Number(n.toString(2).split('').reduce((a,c) => +a + +c,0))
+}
