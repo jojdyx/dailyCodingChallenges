648941a118a6b63e89a6c130dfa3345af317f348
jojdyx <jojdyx@gmail.com>
2022-06-25 23:55:52 -0400

    1 7kyu kata for 2022-06-25

--- 2022-06-25.js
+++ 2022-06-25.js
@@ -8,3 +8,18 @@
 // N will never be less than 1.
 // Method calling example:
 // fizzbuzz(3) -->  [1, 2, "Fizz"]
+function fizzbuzz(n){
+    let arr = []
+    for(let i = 1; i<=n; i++){
+      if(i%3==0 && i%5==0){
+        arr.push('FizzBuzz')
+      }else if(i%3==0){
+        arr.push('Fizz')
+      }else if(i%5==0){
+        arr.push('Buzz')
+      }else{
+        arr.push(i)
+      }
+    }
+    return arr
+}  
